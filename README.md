6180c6ef8d37561c3592f8629c2947f753022e01
arsen-777 <arsengrigoryan3103@gmail.com>
2021-12-02 19:17:00 +0400

    I send you again my homework

---
+++ README.md
@@ -0,0 +1,31 @@
+# Task 1
+
+## Working with variables
+
+. Declare two variables: admin and name.
+. Write the string "John" to the name variable.
+. Copy the value from the name variable to admin.
+. Display the value admin.
+
+# Task 2
+
+## What will be output of i and n?
+
+let i = 10;
+let n = i++ % 5;
+
+# Task 3
+
+## Swap two variables
+
+let a = 10,
+b = 3;
+// a = 3, b =10
+
+# Task 4
+
+## Swap two variables without using the third one
+
+let a = 10,
+b = 3;
+// a = 3, b =10
---
+++ js/index.js
@@ -0,0 +1,42 @@
+// Working with variables start
+
+// let name;
+// let admine;
+// name = "John";
+// admine = name;
+// console.log(admine);
+
+// Working with variables end
+
+// What will be output of i and n? start
+
+// let i = 10;
+// let n = i++ % 5;
+
+// i output 11;
+// n output 0;
+
+// What will be output of i and n? end
+
+// Swap two variables start
+
+// let a = 10;
+// b = 3;
+// let c;
+// c = a;
+// a = b;
+// b = c;
+
+// Swap two variables end
+
+//Swap two variables without using the third one start
+
+// let a = 10;
+// b = 3;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a);
+// console.log(b);
+
+//Swap two variables without using the third one end
